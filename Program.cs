9c85d89692d84475904f1cee5cb067cc89568a8c
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-03-12 09:08:37 +0200

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string wordToRemove = Console.ReadLine().ToLower();
+            string text = Console.ReadLine();
+
+            while (true)
+            {
+                if (!text.Contains(wordToRemove))
+                {
+                    break;
+                }
+
+                text = text.Replace(wordToRemove, string.Empty);
+            }            
+
+            Console.WriteLine(text);
+        }
+    }
+}
